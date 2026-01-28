7021074ddc998fbfa91b5c52b1f67e69a014e2b3
Phoebus Paraskevopoulos <foivos00parask@gmail.com>
2026-01-28 11:22:31 +0200

    Remove config module from imports and __all__ in core package

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
 """Core package for stock price prediction with news sentiment."""
-from . import baselines, checkpoint, data, models, training, utils, config
+from . import baselines, checkpoint, data, models, training, utils
 
 __version__ = "1.0.0"
-__all__ = ["baselines", "checkpoint", "data", "models", "training", "utils", "config"]
+__all__ = ["baselines", "checkpoint", "data", "models", "training", "utils"]
